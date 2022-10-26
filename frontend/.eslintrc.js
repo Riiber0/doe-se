1ebe12441edc2b85bb6f9d23a2a9f30d2295f963
Tales Miguel <tales.miguel@buser.com.br>
2022-10-26 01:15:12 -0300

    lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'vue/order-in-components': 'off',
     'vue/require-default-prop': 'off',
     'vue/require-prop-types': 'off',
-    'vue/no-v-html': 'off'
+    'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
